7e2dcfa29cae54522ff52bbbb2a491b678ead82d
janegunnell <janegunnell1@gmail.com>
2023-04-06 14:12:57 -0700

    this is a file

---
+++ Jane_testingqutip.py
@@ -0,0 +1,21 @@
+
+from qutip import (Qobj, about, basis, coherent, coherent_dm, create, destroy,
+                   expect, fock, fock_dm, mesolve, qeye, sigmax, sigmay,
+                   sigmaz, tensor, thermal_dm)
+import matplotlib.pyplot as plt
+import numpy as np
+from IPython.display import Image
+#data=[2,3,4,5,6]
+
+#plt.plot(data , data)
+#plt.show()
+
+#q = Qobj([[1], [0]])
+
+##print (q)
+
+#print (q.dims)
+
+print (sigmax())
+
+
